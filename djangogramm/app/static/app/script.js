135f119730f66123e2b87e548c179b9b77aa8106
Ais <aisyesm@mail.ru>
2021-12-08 16:12:19 +0500

    Optimize sending get posts request when already showing all user's posts

--- djangogramm/app/static/app/script.js
+++ djangogramm/app/static/app/script.js
@@ -6,13 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
     let start = 0
     let amount = 9
     let user_id = curScriptElement.getAttribute('user_id')
+    let post_count = curScriptElement.getAttribute('post_count')
     getPosts(start, amount, user_id)
 
     window.addEventListener('scroll', function() {
         let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom
         if (windowRelativeBottom < document.documentElement.clientHeight + 50) {
             start += amount
-            getPosts(start, amount, user_id)
+            if (start < post_count) {
+                getPosts(start, amount, user_id)
+            }
         }
     })
 })
