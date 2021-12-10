786207e18821dc746d50f13ebef600b651d29f2f
Ais <aisyesm@mail.ru>
2021-12-10 13:38:13 +0500

    Display post publication date as time delta

--- djangogramm/app/serializers.py
+++ djangogramm/app/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import pytz
 from rest_framework import serializers
 from .models import Post
 
@@ -11,10 +13,33 @@ class UserProfilePostSerializer(serializers.ModelSerializer):
 class FeedPostSerializer(serializers.ModelSerializer):
     user_avatar = serializers.ImageField(source='user.avatar', read_only=True)
     user_email = serializers.EmailField(source='user.email', read_only=True)
-    year = serializers.IntegerField(source='pub_date.year')
-    month = serializers.IntegerField(source='pub_date.month')
-    day = serializers.IntegerField(source='pub_date.day')
 
     class Meta:
         model = Post
-        fields = ['image', 'caption', 'pub_date', 'year', 'month', 'day', 'user', 'user_avatar', 'user_email']
+        fields = ['image', 'caption', 'pub_date', 'user', 'user_avatar', 'user_email']
+
+    def to_representation(self, instance):
+        """Convert `pub_date` to time delta."""
+        ret = super().to_representation(instance)
+        pub_date = datetime.fromisoformat(ret['pub_date'])
+        now = datetime.now(pytz.timezone('Asia/Oral'))
+        diff = now - pub_date
+        if diff.days == 0:
+            if diff.seconds >= 3600:
+                hours_ago = diff.seconds // 3600
+                ret['pub_date'] = f"{hours_ago} hour ago" if hours_ago == 1 else f"{hours_ago} hours ago"
+            elif 60 <= diff.seconds < 3600:
+                min_ago = diff.seconds // 60
+                ret['pub_date'] = f"{min_ago} minute ago" if min_ago == 1 else f"{min_ago} minutes ago"
+            elif diff.seconds < 60:
+                ret['pub_date'] = f"seconds ago" if diff.seconds < 10 else f"{diff.seconds} seconds ago"
+        elif 1 <= diff.days <= 6:
+            ret['pub_date'] = f'{diff.days} day ago' if diff.days == 1 else f'{diff.days} days ago'
+        elif diff.days == 7:
+            ret['pub_date'] = f'1 week ago'
+        elif diff.days > 7:
+            day_of_month = pub_date.strftime('%d').lstrip('0')
+            month = pub_date.strftime('%B')
+            year = pub_date.strftime('%Y')
+            ret['pub_date'] = f"{month} {day_of_month}" if diff.days < 365 else f"{month} {day_of_month}, {year}"
+        return ret
--- djangogramm/app/static/app/feed.js
+++ djangogramm/app/static/app/feed.js
@@ -5,6 +5,7 @@ const curScriptElement = document.currentScript
 document.addEventListener("DOMContentLoaded", function() {
     let start = 0
     const offset = 7
+    getPosts(start, offset)
     const total_num_posts = curScriptElement.getAttribute('total_num_posts')
 
     window.addEventListener('scroll', function() {
@@ -19,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function getPosts (start, offset) {
-    console.log(`making fetch start = ${start} offset = ${offset}`)
     fetch(`http://127.0.0.1:8000/app/posts?offset=${offset}&start=${start}`)
     .then (response => response.json())
     .then (json => showPosts(json))
@@ -63,28 +63,15 @@ function showPosts (posts) {
         const pCaption = document.createElement('p')
         const spanCaption = document.createElement('span')
         spanCaption.classList.add('author')
-        spanCaption.appendChild(userLink)
+        const userLink1 = userLink.cloneNode(true)
+        spanCaption.appendChild(userLink1)
         pCaption.append(spanCaption, ` ${post.caption}`)
         divCaption.append(pCaption)
 
         const divDate = document.createElement('div')
         divDate.classList.add('date')
         const pDate = document.createElement('p')
-        const months = {
-                            1: "January",
-                            2: "February",
-                            3: "March",
-                            4: "April",
-                            5: "May",
-                            6: "June",
-                            7: "July",
-                            8: "August",
-                            9: "September",
-                            10: "October",
-                            11: "November",
-                            12: "December"
-                        }
-        pDate.textContent = `${months[post.month]} ${post.day}, ${post.year}`
+        pDate.textContent = `${post.pub_date}`
         divDate.append(pDate)
 
         divPost.append(divUserPost)
