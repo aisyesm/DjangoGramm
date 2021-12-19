dc11a698a34bbd1225fd5cc29ff1c666185b9507
Ais <aisyesm@mail.ru>
2021-12-19 23:27:26 +0600

    Prepare for deploy

--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -29,9 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
