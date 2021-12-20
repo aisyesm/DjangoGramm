7cdbc458f9eb1fc4ca948b4c96d50ec1a9c958c9
Ais <aisyesm@mail.ru>
2021-12-20 18:29:47 +0600

    Add static_root

--- djangogramm/djangogramm/settings/prod.py
+++ djangogramm/djangogramm/settings/prod.py
@@ -139,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = BASE_DIR / 'app/static/'
+STATIC_ROOT = BASE_DIR / 'app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
