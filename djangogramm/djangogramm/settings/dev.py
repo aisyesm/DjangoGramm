d2024edc62ec6d3d100e0d82baf278f5cb84c213
Ais <aisyesm@mail.ru>
2022-01-05 02:14:19 +0500

    Make user detail page responsive and add sticky navbar

--- djangogramm/djangogramm/settings/dev.py
+++ djangogramm/djangogramm/settings/dev.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('LOCAL_NETWORK'), '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('LOCAL_NETWORK'), os.getenv('LOCAL_NETWORK_2'), '127.0.0.1']
 
 # Application definition
 
