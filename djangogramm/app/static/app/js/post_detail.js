f334eae4a8b199cca1df653b34059bf784f8d7f5
Ais <aisyesm@mail.ru>
2022-01-28 14:05:40 +0500

    Display heart as liked or not liked

--- djangogramm/app/static/app/js/post_detail.js
+++ djangogramm/app/static/app/js/post_detail.js
@@ -4,6 +4,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('js loaded')
     const liked = JSON.parse(document.getElementById('liked').textContent)
-    console.log(liked)
+    const i = document.getElementById('id-heart')
+    setHeart(i, liked)
 
 })
+
+
+function setHeart (i, liked) {
+    if (!liked) {
+        i.className = 'far fa-heart'
+    }
+    else {
+        i.className = 'fas fa-heart red-heart'
+    }
+}
